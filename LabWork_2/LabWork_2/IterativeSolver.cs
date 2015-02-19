c0ff9457464768dc5864ded83326b67e35b4331b
Alexandr Fridman <AlexFridman@outlook.com>
2015-02-19 23:08:53 +0300

    Завершена реализация метода простых итераций.

--- LabWork_2/LabWork_2/IterativeSolver.cs
+++ LabWork_2/LabWork_2/IterativeSolver.cs
@@ -9,9 +9,7 @@ namespace LabWork_2
     public class IterativeSolver
     {
         private readonly Matrix<double> _matrixA;
-        private Matrix<double> _matrixB;
-        private Vector<double> _vectorB;
-        private Vector<double> _vectorC;
+        private readonly Vector<double> _vectorB;
 
         private int _decimals;
         private double _accuracy;
@@ -19,14 +17,15 @@ namespace LabWork_2
         public int Decimals
         {
             get { return _decimals; }
-            set {
-                if (value < 0)
+            set
+            {
+                if(value < 0)
                 {
                     throw new ArgumentException("Decimals must be equal or greather than zero.");
                 }
 
                 _decimals = value;
-                _accuracy = 1/Math.Pow(10, value);
+                _accuracy = 1 / Math.Pow(10, value);
             }
         }
 
@@ -39,47 +38,46 @@ namespace LabWork_2
 
         private void Initialize()
         {
-            Decimals = 4;            
-            CalcMatrixB();
-            CalcVectorC();
+            Decimals = 4;
         }
 
-        private void CalcMatrixB()
+        private Vector<double> GetZeroVector(int length)
         {
-            var eMatrix = DenseMatrix.CreateDiagonal(_matrixA.RowCount, _matrixA.ColumnCount, 1);
-            _matrixB = eMatrix - _matrixA;
-            for (int i = 0; i < _matrixB.RowCount; i++)
-            {
-                for (int j = 0; j < _matrixB.ColumnCount; j++)
-                {
-                    _matrixB[i, j] /= _matrixA[i, i];
-                }
-            }
             const double zero = 0;
-            _matrixB.SetDiagonal(Enumerable.Repeat(zero, _matrixB.RowCount).ToArray());            
-        }
-
-        private void CalcVectorC()
-        {
-            _vectorC = _vectorB / _matrixA.Diagonal();
+            return new DenseVector(Enumerable.Repeat(zero, _matrixA.RowCount).ToArray());
         }
 
         public Vector<double> Solve()
         {
-            var oldX = CalcNewXVector(_matrixB, _vectorC, _vectorC);
-            var curX = CalcNewXVector(_matrixB, _vectorC, oldX);
-            while (!IsAccuracyReached(oldX, curX, _accuracy))
+            var oldX = GetZeroVector(_matrixA.RowCount);
+            var curX = CalcNewXVector(_matrixA, _vectorB, oldX);
+            while(!IsAccuracyReached(oldX, curX, _accuracy))
             {
                 oldX = curX;
-                curX = CalcNewXVector(_matrixB, _vectorB, oldX);
+                curX = CalcNewXVector(_matrixA, _vectorB, oldX);
             }
 
             return curX;
         }
 
-        private Vector<double> CalcNewXVector(Matrix<double> B, Vector<double> C, Vector<double> oldX)
+        private Vector<double> CalcNewXVector(Matrix<double> A, Vector<double> b, Vector<double> oldX)
         {
-            return B*oldX + C;
+            var currentVariableValues = b.Clone();
+
+            for(int i = 0; i < A.RowCount; i++)
+            {
+
+                for(int j = 0; j < A.RowCount; j++)
+                {
+                    if(i != j)
+                    {
+                        currentVariableValues[i] -= A[i, j] * oldX[j];
+                    }
+                }
+            }
+            currentVariableValues /= A.Diagonal();
+
+            return currentVariableValues;
         }
 
         private bool IsAccuracyReached(Vector<double> arg1, Vector<double> arg2, double accuracy)
---
+++ LabWork_2/LabWork_2/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MathNet.Numerics.LinearAlgebra;
+using MathNet.Numerics.LinearAlgebra.Double;
+
+namespace LabWork_2
+{
+    class Program
+    {
+        private static Matrix<double> A;
+        private static Vector<double> b;
+        private const int K = 28;
+        static void Main(string[] args)
+        {
+            Initialize();
+
+            IterativeSolver solver = new IterativeSolver(A, b);
+            Console.WriteLine(solver.Solve());
+            SaveMatrixToFile(A, "A.txt");
+
+        }
+
+        static void Initialize()
+        {
+            var matrixC = ReadMatrixFromFile("C.txt");
+            var matrixD = ReadMatrixFromFile("D.txt");
+            
+            A = K * matrixC + matrixD;
+            
+
+            b = ReadVectorFromFile("b.txt");
+        }
+
+        private static DenseMatrix ReadMatrixFromFile(string filePath)
+        {
+            var matrixLines = File.ReadLines(filePath);
+            var matrixNumLines =
+                matrixLines.Select(
+                    l => l.Split(new[] {' '}, StringSplitOptions.RemoveEmptyEntries).Select(double.Parse));
+            var matrixC = DenseMatrix.OfRows(matrixNumLines);
+            return matrixC;
+        }
+
+        private static DenseVector ReadVectorFromFile(string filePath)
+        {
+            var vectorStr = File.ReadLines(filePath).FirstOrDefault();
+            if (vectorStr == null) throw new InvalidOperationException();
+            var vectorNum = vectorStr.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries).Select(double.Parse);
+            return DenseVector.OfEnumerable(vectorNum);
+        }
+
+        private static void SaveMatrixToFile(Matrix<double> matrix, string path)
+        {
+            using(var stream = File.CreateText(path))
+            {
+                foreach (double[] row in matrix.ToRowArrays())
+                {
+                    StringBuilder rowString = new StringBuilder();
+                    foreach (double item in row)
+                    {
+                        rowString.AppendFormat("{0} ", item.ToString(new CultureInfo("en-US")));
+                    }
+                    stream.WriteLine(rowString);
+                }
+            }
+        }
+    }
+}
