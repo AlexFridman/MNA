33e15890113e5e84f357089bb56e06fa8d626807
Alexandr Fridman <AlexFridman@outlook.com>
2015-03-06 21:32:56 +0300

    Commit before sync

--- LabWork_4/LabWork_4/Program.cs
+++ LabWork_4/LabWork_4/Program.cs
@@ -16,8 +16,8 @@ namespace LabWork_4
             var res = newton.Solve(0.4, 0.7);
             Console.WriteLine(res);
 
-             var newton1 = new NewtonSolver();
-             res = newton1.DefRoots(0.4, 0.7);
+            var newton1 = new NewtonSolver();
+            res = newton1.DefRoots(0.4, 0.7);
             Console.WriteLine(res);
         }
     }
@@ -27,7 +27,7 @@ namespace LabWork_4
         private readonly double _accuracy = 0.0001;
         private static readonly double a = 0.6;
         private static readonly double m = 0.1;
-        private readonly Func<double, double, double> equation1 = (x, y) => Math.Tan(x * y + m);
+        private readonly Func<double, double, double> equation1 = (x, y) => Math.Tan(x*y + m);
         private readonly Func<double, double, double> equation2 = (x, y) => Math.Sqrt(1 - a*Math.Pow(x, 2)/2);
 
         public DenseVector Solve(double x0, double y0)
@@ -44,11 +44,12 @@ namespace LabWork_4
 
                 xN = equation1(x, y);
                 yN = equation2(x, y);
-            } while(yN - y > _accuracy && xN - x > _accuracy);
+            } while (yN - y > _accuracy && xN - x > _accuracy);
 
-            return new DenseVector(new[] { xN, yN });
+            return new DenseVector(new[] {xN, yN});
         }
     }
+
     internal class NewtonSolver
     {
         private readonly double _accuracy = 0.0001;
@@ -60,18 +61,6 @@ namespace LabWork_4
         private readonly Func<double, double, double> equationDer2X = (x, y) => 2*a*x;
         private readonly Func<double, double, double> equationDer1Y = (x, y) => x/Math.Pow(Math.Cos(x*y + m), 2);
         private readonly Func<double, double, double> equationDer2Y = (x, y) => 4*y;
-
-
-        private Vector<double> LinearSystem(double x, double y)
-        {
-            var A = Matrix<double>.Build.DenseOfArray(new[,]
-            {
-                {equationDer1X(x, y), equationDer1Y(x, y)},
-                {equationDer2X(x, y), equationDer2Y(x, y)}
-            });
-            var b = Vector<double>.Build.Dense(new[] {-equation1(x, y), -equation2(x, y)});
-            return A.Solve(b);
-        }
 
         public DenseVector DefRoots(double x0, double y0)
         {
@@ -90,6 +79,17 @@ namespace LabWork_4
             } while (yN - y > _accuracy && xN - x > _accuracy);
 
             return new DenseVector(new[] {xN, yN});
+        }
+
+        private Vector<double> LinearSystem(double x, double y)
+        {
+            var A = Matrix<double>.Build.DenseOfArray(new[,]
+            {
+                {equationDer1X(x, y), equationDer1Y(x, y)},
+                {equationDer2X(x, y), equationDer2Y(x, y)}
+            });
+            var b = Vector<double>.Build.Dense(new[] {-equation1(x, y), -equation2(x, y)});
+            return A.Solve(b);
         }
     }
 }
