711e9c7ceaec4061dd617c9238cadd203cff924e
Alexandr Fridman <AlexFridman@outlook.com>
2015-03-20 23:21:45 +0300

    Lab_6 added.

---
+++ Fridman_MNA/LabWork_6/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Linq;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        private static void Main()
+        {
+            const int size = 11;
+            var xValues = Enumerable.Range(0, 11).Select(i => ((double)i) / 10).ToArray();
+            var yValues = new[] { 1, 1.41, 1.79, 2.13, 2.46, 2.76, 3.04, 3.3, 3.55, 3.79, 4.01 };
+
+
+            Console.WriteLine(InterpolateLagrangePolynomial(0.47, xValues, yValues, size));
+        }
+
+        static double InterpolateLagrangePolynomial(double x, double[] xValues, double[] yValues, int size)
+        {
+            double lagrangePol = 0;
+
+            for(int i = 0; i < size; i++)
+            {
+                double basicsPol = 1;
+                for(int j = 0; j < size; j++)
+                {
+                    if(j != i)
+                    {
+                        basicsPol *= (x - xValues[j]) / (xValues[i] - xValues[j]);
+                    }
+                }
+                lagrangePol += basicsPol * yValues[i];
+            }
+
+            return lagrangePol;
+        }
+
+        static double TestF(double x)
+        {
+            return x * x * x + 3 * x * x + 3 * x + 1; // for example
+        }
+    }
+}
--- Fridman_MNA/LabWork_6/Properties/AssemblyInfo.cs
+++ Fridman_MNA/LabWork_6/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("LabWork_1")]
+[assembly: AssemblyTitle("LabWork_6")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("LabWork_1")]
+[assembly: AssemblyProduct("LabWork_6")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("d98b9176-2eee-4c76-ae62-020107c8f8b2")]
+[assembly: Guid("e4d1bc3f-00af-457c-9992-449de5a0ab4c")]
 
 // Version information for an assembly consists of the following four values:
 //
