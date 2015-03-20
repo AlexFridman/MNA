93e7c2b90eadc5e1e2052c35223b4ba8da8973b5
Alexandr Fridman <AlexFridman@outlook.com>
2015-03-20 23:39:06 +0300

    Lab_6 implemented.

--- Fridman_MNA/LabWork_6/Program.cs
+++ Fridman_MNA/LabWork_6/Program.cs
@@ -5,14 +5,17 @@ namespace ConsoleApp
 {
     class Program
     {
+        private static double[] xValues = Enumerable.Range(0, 11).Select(i => ((double)i) / 10).ToArray();
+        private static double[] yValues = new[] { 1, 1.41, 1.79, 2.13, 2.46, 2.76, 3.04, 3.3, 3.55, 3.79, 4.01 };
+        const int size = 11;
         private static void Main()
         {
-            const int size = 11;
-            var xValues = Enumerable.Range(0, 11).Select(i => ((double)i) / 10).ToArray();
-            var yValues = new[] { 1, 1.41, 1.79, 2.13, 2.46, 2.76, 3.04, 3.3, 3.55, 3.79, 4.01 };
+            
+           
 
 
-            Console.WriteLine(InterpolateLagrangePolynomial(0.47, xValues, yValues, size));
+            Console.WriteLine(InterpolateLagrangePolynomial(47, xValues, yValues, size));
+            Console.WriteLine(Newton(47, xValues, yValues, size));
         }
 
         static double InterpolateLagrangePolynomial(double x, double[] xValues, double[] yValues, int size)
@@ -35,9 +38,31 @@ namespace ConsoleApp
             return lagrangePol;
         }
 
-        static double TestF(double x)
+        static double Newton(double x, double[] xValues, double[] yValues, int size)
         {
-            return x * x * x + 3 * x * x + 3 * x + 1; // for example
+            double res = yValues[0], F, den;
+            int i, j, k;
+            for(i = 1; i < size; i++)
+            {
+                F = 0;
+                for(j = 0; j <= i; j++)
+                {//следующее слагаемое полинома
+                    den = 1;
+                    //считаем знаменатель разделенной разности
+                    for(k = 0; k <= i; k++)
+                    {
+                        if(k != j)
+                            den *= (xValues[j] - xValues[k]);
+                    }
+                    //считаем разделенную разность
+                    F += yValues[j] / den;
+                }
+                //домножаем разделенную разность на скобки (x-x[0])...(x-x[i-1])
+                for(k = 0; k < i; k++)
+                    F *= (x - xValues[k]);
+                res += F;//полином
+            }
+            return res;
         }
     }
 }
